b22589d71224e7d2a72cdb0d56a4beb26f52f7d6
pedro_bras <pedro.c.bras@gmail.com>
2019-09-03 14:40:57 +0200

    deletes fake test

--- cypress/integration/testTest.spec.js
+++
@@ -1,7 +0,0 @@
-describe("Visitor have a functional landing page", () => {
-  it("Should see phadder when visiting page", () => {
-    cy.server();
-    cy.visit("http://localhost:3001");
-    cy.contains("Phadder");
-  });
-});
