1a70317437657589dd1f5e47a8a3000cce2e26b2
pedro_bras <pedro.c.bras@gmail.com>
2019-09-03 20:31:27 +0200

    created buyers and suppliers explain, added info to buyers

---
+++ src/components/SuppliersExplain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Phadder</h1>
+    </Container>
+  );
+};
+
+export default Home;
