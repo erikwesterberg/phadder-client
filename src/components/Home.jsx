d123986b464eca342bdf8d85b3b5d6cf20b01933
pedro_bras <pedro.c.bras@gmail.com>
2019-09-03 20:49:39 +0200

    adds state to Home component with hooks, adds buttons for buyer and suplier and changes state to display info

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "semantic-ui-react";
 import BuyersExplain from "./BuyersExplain";
 import SuppliersExplain from "./SuppliersExplain";
 
 const Home = () => {
+  const [explain, setExplain] = useState(<BuyersExplain />);
   return (
     <Container>
       <h1>Phadder</h1>
-      <BuyersExplain />
-      <SuppliersExplain />
+      <button onClick={() => setExplain(<BuyersExplain />)}>
+        Buyers
+      </button>
+      <button onClick={() => setExplain(<SuppliersExplain />)}>
+        Suppliers
+      </button>
+      {explain}
     </Container>
   );
-}
+};
 
 export default Home;
