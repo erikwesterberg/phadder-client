65a578571aefb337e6ea706abe9f7e86419abd0c
erka <erik.westerberg@hotmail.se>
2019-09-03 20:38:25 +0200

    scaffolds basic supplier component and render supplier and buyer component in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import BuyersExplain from "./BuyersExplain";
+import SuppliersExplain from "./SuppliersExplain";
 
 const Home = () => {
   return (
     <Container>
       <h1>Phadder</h1>
+      <BuyersExplain />
+      <SuppliersExplain />
     </Container>
   );
 }
