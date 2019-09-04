ee616bf0d43f16dfd72ea06a17a47491870239b0
pedro_bras <pedro.c.bras@gmail.com>
2019-09-04 10:34:16 +0200

    adds signup form to signup component, fixes feature test file name

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,48 @@
 import React from "react";
-import { Button } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 
 
 const SignUp = () => {
   return (
-    <div >
-    <Button>Sign Up</Button>
+    <div>
+      <Form id="signup-form">
+        <Form.Field>
+          <label id="first-name">First Name</label>
+          <input id="name" />
+        </Form.Field>
+
+        <Form.Field>
+          <label id="last-name">Last Name</label>
+          <input id="name" />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Email</label>
+          <input
+            id="email"
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Password</label>
+          <input
+            id="password"
+            type="password"
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Password Confirmation</label>
+          <input
+            id="password-confirmation"
+            type="password"
+          />
+        </Form.Field>
+
+        <Button id="submit-account-button" type="submit">
+          Sign Up
+        </Button>
+      </Form>
     </div>
   );
 }
