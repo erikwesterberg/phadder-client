306cfa7e2700d1ce5e958abee23cae8ec0b11c6f
pedro_bras <pedro.c.bras@gmail.com>
2019-09-04 10:54:18 +0200

    adds sign up form as semantic modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
-import { Container, Button } from "semantic-ui-react";
+import { Container, Button, Modal } from "semantic-ui-react";
 import BuyersExplain from "./BuyersExplain";
 import SuppliersExplain from "./SuppliersExplain";
 import SignUp from "./SignUp";
 
 const Home = () => {
   const [explain, setExplain] = useState(<BuyersExplain />);
-  const [signupForm, setSignUpForm ] = useState();
 
   return (
     <Container>
-      <Button id="sign-up-button" onClick={() => setSignUpForm(<SignUp />)}>REGISTER AS A CLIENT</Button>
-      {signupForm}
-      <h1>Phadder</h1>
-      <Button id="buyers-button" onClick={() => setExplain(<BuyersExplain />)}>Buyers</Button>
-      <Button id="suppliers-button" onClick={() => setExplain(<SuppliersExplain />)}>Suppliers</Button>
+      <div>
+        <Modal
+          trigger={<Button id="sign-up-button">REGISTER AS A CLIENT</Button>}
+          centered={false}
+        >
+          <Modal.Header>Join us!</Modal.Header>
+          <Modal.Content>
+            <SignUp />
+          </Modal.Content>
+        </Modal>
+      </div>
+      <div>
+        <Button
+          id="buyers-button"
+          onClick={() => setExplain(<BuyersExplain />)}
+        >
+          Buyers
+        </Button>
+        <Button
+          id="suppliers-button"
+          onClick={() => setExplain(<SuppliersExplain />)}
+        >
+          Suppliers
+        </Button>
+      </div>
       {explain}
     </Container>
   );
