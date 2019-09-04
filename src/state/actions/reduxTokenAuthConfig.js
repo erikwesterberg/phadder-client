7ddffef115cf514257accce23ad1d4bbf54c468b
pedro_bras <pedro.c.bras@gmail.com>
2019-09-04 13:39:22 +0200

    adds redux token auth config file, adds configurestore file, adds redux to index.js, adds logger and thunk

---
+++ src/state/actions/reduxTokenAuthConfig.js
@@ -0,0 +1,27 @@
+import { generateAuthActions } from "redux-token-auth";
+
+let authUrl = "http://localhost:3000/api/auth";
+
+const config = {
+  authUrl,
+  userAttributes: {
+    firstName: "first_name",
+    lastName: "last_name",
+    email: "email"
+  },
+  userRegistrationAttributes: {
+    firstName: "first_name",
+    lastName: "last_name",
+    email: "email",
+    password_confirmation: "password_confirmation"
+  }
+};
+
+const {
+  registerUser,
+  signInUser,
+  signOutUser,
+  verifyCredentials
+} = generateAuthActions(config);
+
+export { registerUser, signInUser, signOutUser, verifyCredentials };
