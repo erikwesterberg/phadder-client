35fa209a793c5d6587b950d37f682b224e20fa2f
pedro_bras <pedro.c.bras@gmail.com>
2019-09-05 10:57:15 +0200

    creates login form component

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button, Form, Modal } from "semantic-ui-react";
+import useForm from "react-hook-form";
+import { connect } from "react-redux";
+import { signInUser } from "../redux/actions/reduxTokenAuthConfig";
+
+const LogIn = props => {
+
+}
+
+export default LogIn
