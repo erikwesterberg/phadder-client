92087cb293e1f1b7ef2d13067505baa64887f94b
pedro_bras <pedro.c.bras@gmail.com>
2019-09-09 18:07:20 +0200

    adds configs to i18n js file

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,20 @@ import Backend from "i18next-xhr-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const fallbackLanguage = ['en'];
-const availableLanguages = ['en', 'sv'];
+const fallbackLanguage = ["en"];
+const availableLanguages = ["en", "sv"];
 
 i18n
   .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLanguage,
+    debug: true,
+    whitelist: availableLanguages,
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
