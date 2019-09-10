67154c48901b98c13c80a91e05ac68feb1691950
erka <erik.westerberg@hotmail.se>
2019-09-10 15:44:19 +0200

    taking help of useState to make a get req for the city trough zipcode

--- src/components/CityFetcher.jsx
+++ src/components/CityFetcher.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Modal, Form } from "semantic-ui-react";
 import "../css/style.css";
 
-const CityFetcher = () => (
+const CityFetcher = () => {
+ const [cityFetch, setCityFetch] = useState()
+ const [city, newCity] = useState()
+
+ async getArticles(cityFetch) {
+  const response = await axios.get(`/city/${cityFetch}`);;
+  if (response.data.length > 0) { 
+    newCity(city)
+    };
+  }
+
+  return (
   <Modal
     size="mini"
     trigger={<Button id="get-location-button">GET LOCATION</Button>}
@@ -12,14 +23,16 @@ const CityFetcher = () => (
       <Modal.Description>
         <Form>
           <Form.Field>
-            <input id="post-code-input" placeholder="Enter your post code" />
+            <input id="post-code-input" placeholder="Enter your post code" onChange={setCityFetch(cityFetch)} />
           </Form.Field>
         </Form>
       </Modal.Description>
+      {city}
       <Button>GET STARTED</Button>
       <Button>CANCEL</Button>
     </Modal.Content>
   </Modal>
-);
+    )
+  };
 
 export default CityFetcher;
