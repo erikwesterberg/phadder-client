27a2744c239573bd03c28b1ac3a48a89c8612d33
erka <erik.westerberg@hotmail.se>
2019-09-10 12:21:37 +0200

    wraps supplier in grid columns and imports needed images

--- src/components/SuppliersExplain.jsx
+++ src/components/SuppliersExplain.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Grid } from "semantic-ui-react";
 import "../css/style.css";
 import { I18nContext } from "../i18n/index";
 
@@ -15,43 +15,66 @@ const SuppliersExplain = () => {
         <h1>{translate("supplier-banner")}</h1>
       </div>
 
-      <h1>Join the elite</h1>
-      <p>
-        Complete an online application to join the exclusive Phadder Service
-        Supplier community.
-      </p>
-      <p>
-        As a Service Supplier, you'll gain access to a stream of project
-        requests from our international client community. Now you’re ready to
-        provide your services to new clients in your area - Better, Safer and
-        More Profitable.
-      </p>
-      <p>
-        It's all within your reach! Register during the Pre Release period and
-        get 50% discount on the Verified Supplier Badge (optional)
-      </p>
-      <h1>Build your reputation</h1>
-      <p>
-        Networks and referrals are the main reasons for accepting bids and
-        making purchases. With Phadder, your business will be able to build an
-        online reputation and grow your business.
-      </p>
-      <p>
-        Make diversity into a competitive advantage. Benefit from the
-        multi-cultural composition of your team. The unique multi-language
-        approach of Phadder makes it possible for your clients to communicate
-        with you in their native language.
-      </p>
-      <h1>The decisive advantage</h1>
-      <p>
-        A basic membership on Phadder is FREE, but even the paid Verified
-        Supplier Badge costs significantly less than many other classic sales
-        channels and can generate up to three times as many leads.
-      </p>
-      <p>
-        In addition, the majority of business owners, prefer to look for
-        suppliers and service providers online.
-      </p>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <div id="supplier1-logo" />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <h1>Join the elite</h1>
+            <p>
+              Select a location and project category that best matches your
+              business need
+            </p>
+            <p>
+              Tell us about your project to ensure we're able to match you to
+              the right pro for your job
+            </p>
+            <p>
+              Phadder has several service providers specializing in over 20
+              business categories
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <div id="supplier2-logo" />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <h1>Build your reputation</h1>
+            <p>
+              Networks and referrals are the main reasons for accepting bids and
+              making purchases. With Phadder, your business will be able to
+              build an online reputation and grow your business.
+            </p>
+            <p>
+              Make diversity into a competitive advantage. Benefit from the
+              multi-cultural composition of your team. The unique multi-language
+              approach of Phadder makes it possible for your clients to
+              communicate with you in their native language.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+
+          <Grid.Row>
+          <Grid.Column width={8}>
+            <div id="supplier3-logo" />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <h1>The decisive advantage</h1>
+            <p>
+              A basic membership on Phadder is FREE, but even the paid Verified
+              Supplier Badge costs significantly less than many other classic
+              sales channels and can generate up to three times as many leads.
+            </p>
+            <p>
+              In addition, the majority of business owners, prefer to look for
+              suppliers and service providers online.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Container>
   );
 };
