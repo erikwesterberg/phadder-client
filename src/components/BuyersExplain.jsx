1204ecbf929d00755bbb25b1535ef4afb830b2df
pedro_bras <pedro.c.bras@gmail.com>
2019-09-10 11:54:42 +0200

    adds buyers explainer part with semantic ui grid

--- src/components/BuyersExplain.jsx
+++ src/components/BuyersExplain.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Grid } from "semantic-ui-react";
 import "../css/style.css";
 import { I18nContext } from "../i18n/index";
 
@@ -15,71 +15,65 @@ const BuyersExplain = () => {
         <h1>{translate("buyer-banner")}</h1>
       </div>
 
-      <div class="ui segment">
-        <div class="ui left aligned container">
-          <div id="buyer1-logo" />
-        </div>
-        <div class="ui right aligned container">
-          <h1>Get Started</h1>
-          <p>
-            Select a location and project category that best matches your
-            business need
-          </p>
-          <p>
-            Tell us about your project to ensure we're able to match you to the
-            right pro for your job
-          </p>
-          <p>
-            Phadder has several service providers specializing in over 20
-            business categories
-          </p>
-        </div>
-      </div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <div id="buyer1-logo" />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <h1>Get Started</h1>
+            <p>
+              Select a location and project category that best matches your
+              business need
+            </p>
+            <p>
+              Tell us about your project to ensure we're able to match you to
+              the right pro for your job
+            </p>
+            <p>
+              Phadder has several service providers specializing in over 20
+              business categories
+            </p>
+          </Grid.Column>
+        </Grid.Row>
 
-      <div class="ui segment">
-        <div class="ui left aligned container">
-          <div id="buyer2-logo" />
-        </div>
-        <div class="ui right aligned container">
-          <h1>Get Connected</h1>
-          <p>
-            Your request will be presented to up to four pre-screened, verified,
-            local Phadder Service Suppliers
-          </p>
-          <p>
-            After 24 h, your request will be listed on our site for other
-            Phadder Service Suppliers to see and respond to
-          </p>
-        </div>
-      </div>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <div id="buyer2-logo" />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <h1>Get Connected</h1>
+            <p>
+              Your request will be presented to up to four pre-screened,
+              verified, local Phadder Service Suppliers
+            </p>
+            <p>
+              After 24 h, your request will be listed on our site for other
+              Phadder Service Suppliers to see and respond to
+            </p>
+          </Grid.Column>
+        </Grid.Row>
 
-      <div class="ui segment">
-        <div class="ui left aligned container">
-          <div id="buyer3-logo" />
-        </div>
-        <div class="ui right aligned container">
-          <h1>Get The Job Done</h1>
-          <p>
-            Shortly after posting your service request, you start receiving bids
-            and proposals to choose from
-          </p>
-          <p>Connect with your selected service providers to negotiate terms</p>
-          <p>
-            Use Phadders Smart E-greements Service to secure the legal agreement
-            with your selected provider (optional)
-          </p>
-        </div>
-      </div>
-
-      <h1>
-        With Phadder, you will find trusted local professionals for pretty much
-        any job or project.
-      </h1>
-      <p>
-        Finding a professional supplier has never been easier. No more cold
-        calling. No mystery pricing. No asking around if somebody “know a guy.”
-        Phadder is a completely free service that does the work for you.
-      </p>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <div id="buyer3-logo" />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <h1>Get The Job Done</h1>
+            <p>
+              Shortly after posting your service request, you start receiving
+              bids and proposals to choose from
+            </p>
+            <p>
+              Connect with your selected service providers to negotiate terms
+            </p>
+            <p>
+              Use Phadders Smart E-greements Service to secure the legal
+              agreement with your selected provider (optional)
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Container>
   );
 };
