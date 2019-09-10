b4427929883fb789915fe07cfaec06ad7c2d0514
pedro_bras <pedro.c.bras@gmail.com>
2019-09-10 16:13:01 +0200

    adds onchange function to input form, fixes axios post request

--- src/components/CityFetcher.jsx
+++ src/components/CityFetcher.jsx
@@ -1,38 +1,52 @@
 import React, { useState } from "react";
 import { Button, Modal, Form } from "semantic-ui-react";
 import "../css/style.css";
+import axios from "axios";
 
 const CityFetcher = () => {
- const [cityFetch, setCityFetch] = useState()
- const [city, newCity] = useState()
+ const [location, setLocation] = useState()
+ const [postCode, setPostCode] = useState()
 
- async getArticles(cityFetch) {
-  const response = await axios.get(`/city/${cityFetch}`);;
-  if (response.data.length > 0) { 
-    newCity(city)
-    };
-  }
+ getLocation = async () => {
+   try {
+     let response = await axios.post("/post_code_queries", postCode);
+     if (response.status === 200) {
+       setLocation(response)
+     }
+   } catch (error) {
+     console.log(error)
+   }
+ };
+
+  const onChangeHandler = event => {
+    setPostCode(event.target.value);
+    getLocation();
+  };
 
   return (
-  <Modal
-    size="mini"
-    trigger={<Button id="get-location-button">GET LOCATION</Button>}
-  >
-    <Modal.Header>Step 1: Enter your post code</Modal.Header>
-    <Modal.Content>
-      <Modal.Description>
-        <Form>
-          <Form.Field>
-            <input id="post-code-input" placeholder="Enter your post code" onChange={setCityFetch(cityFetch)} />
-          </Form.Field>
-        </Form>
-      </Modal.Description>
-      {city}
-      <Button>GET STARTED</Button>
-      <Button>CANCEL</Button>
-    </Modal.Content>
-  </Modal>
-    )
+    <Modal
+      size="mini"
+      trigger={<Button id="get-location-button">GET LOCATION</Button>}
+    >
+      <Modal.Header>Step 1: Enter your post code</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <Form>
+            <Form.Field>
+              <input
+                id="post-code-input"
+                placeholder="Enter your post code"
+                onChange={onChangeHandler()}
+              />
+            </Form.Field>
+          </Form>
+        </Modal.Description>
+        {location}
+        <Button>GET STARTED</Button>
+        <Button>CANCEL</Button>
+      </Modal.Content>
+    </Modal>
+  );
   };
 
 export default CityFetcher;
