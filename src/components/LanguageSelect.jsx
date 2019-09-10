d1dac4768bc5a884940ca58a053711d018fe0489
pedro_bras <pedro.c.bras@gmail.com>
2019-09-10 11:37:37 +0200

    adds styling to language selector

--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -1,25 +1,34 @@
 import React, { useContext } from "react";
+import { Button } from "semantic-ui-react";
+import "../css/style.css";
 
 import { I18nContext } from "../i18n/index";
 
 const LanguageSelect = props => {
   const { langCode, dispatch } = useContext(I18nContext);
 
-  
   const onLanguageSelect = e =>
     dispatch({ type: "setLanguage", payload: e.target.value });
 
-  const renderOption = code => (
-    <option value={code} selected={code === langCode}>
-      {code}
-    </option>
+  const renderOption = (code, display) => (
+    <button
+      id="language-button"
+      onClick={onLanguageSelect}
+      value={code}
+      selected={code === langCode}
+    >
+      {display}
+    </button>
   );
 
   return (
-    <select id="language-select" onChange={onLanguageSelect}>
-      {renderOption("EN")}
-      {renderOption("SV")}
-    </select>
+    <div id="language-select">
+      <Button id="drop-button">Language</Button>
+      <div id="language-options">
+        {renderOption("EN", "English")}
+        {renderOption("SV", "Swedish")}
+      </div>
+    </div>
   );
 };
 
