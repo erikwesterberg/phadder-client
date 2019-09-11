a9858ed0afb85365cf862c11d26eba3e3a888960
erka <erik.westerberg@hotmail.se>
2019-09-11 21:45:26 +0200

    created and example picture in cypress fixture folder for mock purpose

--- cypress/integration/userCanUploadProfileImage.spec.js
+++ cypress/integration/userCanUploadProfileImage.spec.js
@@ -6,16 +6,16 @@ describe("Logged in User can upload profile images", () => {
       url: "http://localhost:3000/api/auth/sign_in",
       response: "fixture:successful_log_in.json"
     });
+    cy.user_login("johndoe@mail.com", "password");      
+  });
+
+  it("User upload images succesfully", () => {
     cy.route({
       method: "POST",
       url: "http://localhost:3000/api/profile_update",
       response: "fixture:successful_uploads_image_response.json",
       status: 200
     });;
-    cy.user_login("johndoe@mail.com", "password");   
-  });
-
-  it("User upload images succesfully", () => {
       cy.get("#profile-button").click();
       cy.contains("Welcome John, to your profile page!");
       cy.get("#select-image").click();
@@ -23,6 +23,5 @@ describe("Logged in User can upload profile images", () => {
       cy.get("#upload-button").click();
       cy.contains("Profile picture successfully updated");
     });
-
   });
 
