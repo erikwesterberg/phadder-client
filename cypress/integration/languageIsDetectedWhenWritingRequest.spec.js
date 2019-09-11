7c977bef04a6b0b6648186311c88df17ee2ff5d7
pedro_bras <pedro.c.bras@gmail.com>
2019-09-11 20:18:48 +0200

    creates feature test

---
+++ cypress/integration/languageIsDetectedWhenWritingRequest.spec.js
@@ -0,0 +1,22 @@
+describe("Client can create service request", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.visit("http://localhost:3001");
+  });
+
+  it("Request is posted successfully", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/language_queries",
+      response: "fixture:successful_language_detection_response.json",
+      status: 200
+    });
+    cy.get("#create-request-button").click();
+    cy.get("#request-form").within(() => {
+      cy.get("#title").type("Build my webpage");
+      cy.get('select[id="category"]').select("IT Service");
+      cy.get("#details").type("My roof just fell down because of the storm and I need someone qualified to fix it. This is an urgent request. ");
+    });
+    cy.contains("You are writing in English...");
+  });
+})
