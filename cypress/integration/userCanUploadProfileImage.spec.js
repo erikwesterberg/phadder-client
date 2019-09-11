f966ab473bb1a6934df8b0ec168b156ceaf6729b
erka <erik.westerberg@hotmail.se>
2019-09-11 22:43:35 +0200

    adds sad path testing for uloading profile picture

--- cypress/integration/userCanUploadProfileImage.spec.js
+++ cypress/integration/userCanUploadProfileImage.spec.js
@@ -1,4 +1,4 @@
-describe("Logged in User can upload profile images", () => {
+describe("Looged in users can update there profile picture", () => {
   beforeEach(() => {
     cy.server()
     cy.route({
@@ -9,11 +9,11 @@ describe("Logged in User can upload profile images", () => {
     cy.user_login("johndoe@mail.com", "password");      
   });
 
-  it("User upload images succesfully", () => {
+  it("User updates profile picture succesfully", () => {
     cy.route({
       method: "POST",
       url: "http://localhost:3000/api/profile_update",
-      response: "fixture:successful_uploads_image_response.json",
+      response: "fixture:successful_uploads_profile_image_response.json",
       status: 200
     });;
       cy.get("#profile-button").click();
@@ -22,7 +22,21 @@ describe("Logged in User can upload profile images", () => {
       cy.get("#select-image").click();
       cy.file_upload("image.jpeg");
       cy.get("#upload-button").click();
-      cy.contains("Profile picture successfully updated");
+      cy.contains("Profile picture successfully uploaded");
     });
-  });
 
+  it("User updates profile picture unsuccesfull", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/profile_update",
+      response: "fixture:unsuccessful_upload_profile_image_response.json",
+      status: 401
+    });
+    cy.get("#profile-button").click();
+    cy.get("#edit-profile-picture").click()
+    cy.get("#select-image").click();
+    cy.file_upload("image.jpeg");
+    cy.get("#upload-button").click(); 
+    cy.contains("Your image was not uploaded, please try again.");
+  });
+});
