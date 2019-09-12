5c1b068fde509e2a70f9d644371ec339e34bb2f9
erka <erik.westerberg@hotmail.se>
2019-09-12 12:50:56 +0200

    sends props trough service req templete and renders it on screen if button is clicked

--- src/components/DisplayServiceRequest.jsx
+++ src/components/DisplayServiceRequest.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { Button } from "semantic-ui-react";
 import axios from 'axios'
+import RequestTemplate from "./ServiceRequestTemplate"
  
 
 const DisplayServiceRequest = () => {
  const [showRequest, setShowRequest] = useState(false)
  const [serviceRequests, setServiceRequest] = useState()
  const [errorMessage, setErrorMessage] = useState()
+ let usersRequests
   
  const getServiceRequest = async e => {
   try {
@@ -21,8 +23,7 @@ const DisplayServiceRequest = () => {
 }
 
 if (showRequest === true) {
-  
-  UsersRequests = (
+ usersRequests = (
     <div id="service-request">
     <h1>Service Requests</h1>
     {serviceRequests.map(serviceReguest => {
@@ -34,7 +35,7 @@ if (showRequest === true) {
   return (
     <>
     <Button id="my-requests-button" onClick={(e) => getServiceRequest(e)}>My requests</Button>
-    {UsersRequests}
+    {usersRequests}
     {errorMessage}
     </>
   )
